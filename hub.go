ea92dd260b83d234cf41b1252560ad4fdb6090f6
Cenk Alti <cenkalti@gmail.com>
2014-01-20 17:47:28 +0200

    update comments

--- hub.go
+++ hub.go
@@ -20,9 +20,7 @@ func New() *Hub {
 	return &Hub{subscribers: make(map[int][]func(Event))}
 }
 
-// Subscribe for the event of specific kind.
-// The caller must receive messages from the retured channel.
-// Otherwise, the next Publish() will hang.
+// Subscribe registers the handler for the event of a specific kind.
 func (h *Hub) Subscribe(kind int, handler func(Event)) {
 	h.m.Lock()
 	h.subscribers[kind] = append(h.subscribers[kind], handler)
@@ -43,7 +41,8 @@ func (h *Hub) Publish(e Event) {
 // DefaultHub is the default Hub used by Publish and Subscribe.
 var DefaultHub = New()
 
-// Subscribe for the event of specific kind in the DefaultHub.
+// Subscribe registers the handler for the event of a specific kind
+// in the DefaultHub.
 func Subscribe(kind int, handler func(Event)) {
 	DefaultHub.Subscribe(kind, handler)
 }
